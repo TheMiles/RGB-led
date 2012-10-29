b0cd419258f3baa349b1ca3d6736805503e69027
The Miles <miles@coders-of-the-caribbean.org>
2012-10-29 23:55:29 +0100

    reorganisation stuff

--- RGB-led.ino
+++ RGB-led.ino
@@ -1,131 +1,160 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
- 
-  This example code is in the public domain.
- */
- 
+// RGB-led
+// Reading values from serial port and setting analog ports
+// with this value
+
+
 // const for LED out
 enum Channel { Red, Green, Blue1, Blue2 };
 
-const int LED[] = { 9, 10, 11, 3 }; // Red led, green, blue1, blue2
-int color[]            = { 255, 128, 0, 0 }; // R,G,B1,B2 values are constrained to [0..255]
+const int LED[]              = { 9, 10, 11, 3 };   // port numbers for channel
+int       color[]            = { 255, 128, 0, 0 }; // values range [0..255]
+int       active_channel     = Red;
+long      previous_timestamp = 0;
+long      base_delay         = 0;
 
-int activeChannel = Red;
 
-long previousTimestamp = 0;
-long baseDelay         = 0;
-int frame              = 0;
+class VerboseSerial
+{
+  public:
+  VerboseSerial()
+  {
+    Serial.begin(9600);      // initialize the serial port
+  }
+
+  bool good()
+  {
+    return Serial.available() > 0;
+  }
+
+  int parse_int()
+  {
+    return Serial.parseInt();
+  }
+
+  char read()
+  {
+    return Serial.read();
+  }
+
+  void flush()
+  {
+    while( good() )
+    {
+      read();
+    }
+  }
+};
+
+
+class Error
+{
+  public:
+  Error()
+  {
+    pinMode( 13, OUTPUT ); // pin 13 for signaling
+  }
+
+  void handle_error( unsigned char error)
+  {
+    modal_blink( error );
+  }
+
+  // blinks the onboard led using the pattern provided
+  // in info
+  // 
+  void modal_blink( unsigned char info )
+  {
+    while( info != 0 ) // each shift decreases the value, without adding 1s
+    {
+       digitalWrite( 13, info & 1 );  // use last bit to set output
+       info = info >> 1;              // shift to the right to have
+                                      // it in position for the next round
+       delay( 100 );
+    }
+    digitalWrite( 13, LOW ); // turn led of, when we are finished
+  }
+};
+
+
+VerboseSerial verbose_serial;
+Error error_handler;
+
 
 // the setup routine runs once when you press reset:
 void setup() {                
-  // initialize the digital pins as output.
-  /* pinMode(LED[Red],   OUTPUT);      */
-  /* pinMode(LED[Green], OUTPUT);      */
-  /* pinMode(LED[Blue1], OUTPUT);      */
-  /* pinMode(LED[Blue2], OUTPUT);   */
-
-  pinMode( 13, OUTPUT );
-  
-  // initialize the serial port
-  Serial.begin( 9600 );
-  modalBlink(); // check if it's running by triggering blinking
+  error_handler.modal_blink( 0x55 );// check if it's running
+                                      // by triggering blinking
 }
 
-void modalBlink()
-{
-  bool blink = true;
 
-  for( int i = 0; i < 10; i++ )
+
+
+bool handle_channel()
+{
+  int tmp_value  = verbose_serial.parse_int();
+  active_channel = constrain( tmp_value, Red, Blue2 );
+  return true;
+}
+
+bool handle_value()
+{
+  int tmp_value = verbose_serial.parse_int();
+  color[ active_channel ] = constrain( tmp_value, 0, 255 );
+  return true;
+}
+
+bool update_current_channel()
+{
+  analogWrite( LED[active_channel],   color[active_channel] );
+  return true;
+}
+
+bool parse_input()
+{
+  bool went_well = false;
+
+  if( verbose_serial.good() )
   {
-    digitalWrite( 13, blink );
-    blink = !blink;
-    delay( 100 );
-  }
-}
-
-void handleError()
-{
-  //modalBlink();
-}
-
-bool good()
-{
-  return Serial.available() > 0;
-}
-
-bool handleChannel()
-{
-  int tmpValue  = Serial.parseInt();
-  activeChannel = constrain( tmpValue, Red, Blue2 );
-  return true;
-}
-
-bool handleValue()
-{
-  int tmpValue = Serial.parseInt();
-  color[ activeChannel ] = constrain( tmpValue, 0, 255 );
-  return true;
-}
-
-bool updateCurrentChannel()
-{
-  analogWrite( LED[activeChannel],   color[activeChannel] );
-  return true;
-}
-
-bool parseInput()
-{
-  bool wentWell = false;
-
-  if( good() )
-  {
-    char command = Serial.read();
+    char command = verbose_serial.read();
     switch( command )
     {
     case 'c':
     case 'C':
-      wentWell = handleChannel();
+      went_well = handle_channel();
       break;
     case 'v':
     case 'V':
-      wentWell = handleValue();
+      went_well = handle_value();
       break;
     default:
-      wentWell = false;
+      went_well = false;
     }
 
-    if( wentWell )
+    if( went_well )
     {
-      wentWell = updateCurrentChannel();
+      went_well = update_current_channel();
     }
 
-    if( !wentWell )
+    if( !went_well )
     {
-      handleError();
+      error_handler.handle_error( 0x05 );
     }
 
     // no matter how it went, skip the rest of the string
-    while( good() )
-    {
-      Serial.read();
-    }
+    verbose_serial.flush();
   }
   
-  return wentWell;
+  return went_well;
 }
-
-bool switcher = false;
-
 
 // the loop routine runs over and over again forever:
 void loop() {
   
-  long currentTimestamp = millis();
-  if( currentTimestamp - previousTimestamp > baseDelay )
+  long current_timestamp = millis();
+  if( current_timestamp - previous_timestamp > base_delay )
   {
-     previousTimestamp = currentTimestamp;
-     parseInput();
+     previous_timestamp = current_timestamp;
+     parse_input();
   }
 }
  
