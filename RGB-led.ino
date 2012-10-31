0921e73f1c4dc09ad0444e49e17bbd558c460a68
The Miles <miles@coders-of-the-caribbean.org>
2012-10-31 02:00:29 +0100

    It works,....

--- RGB-led.ino
+++ RGB-led.ino
@@ -2,15 +2,20 @@
 // Reading values from serial port and setting analog ports
 // with this value
 
-
 // const for LED out
 enum Channel { Red, Green, Blue1, Blue2 };
 
-const int LED[]              = { 9, 10, 11, 3 };   // port numbers for channel
+const int LED[]              = { 9, 11, 10, 3 };   // port numbers for channel
 int       color[]            = { 255, 128, 0, 0 }; // values range [0..255]
 int       active_channel     = Red;
 long      previous_timestamp = 0;
-long      base_delay         = 0;
+long      base_delay         = 10;
+
+const int input_buffer_length  = 64;
+char input_buffer[ input_buffer_length + 1 ];
+char command = 'n';
+int  number = 0;
+int  idle_counter =0;
 
 
 class VerboseSerial
@@ -19,25 +24,68 @@ class VerboseSerial
   VerboseSerial()
   {
     Serial.begin(9600);      // initialize the serial port
+    Serial.println( "Im here" );
+    
   }
 
+  ~VerboseSerial()
+  {
+    Serial.println( "Bye bye..." );
+  }
+  
+  int available()
+  {
+    return Serial.available();
+  }
+  
+          
   bool good()
   {
-    return Serial.available() > 0;
+    int available = Serial.available();
+    
+    Serial.print( "good: " );
+    Serial.println( available );
+    
+    return available > 0;
   }
 
   int parse_int()
   {
-    return Serial.parseInt();
+    int value = Serial.parseInt();
+
+    Serial.print( "Int value: " );
+    Serial.println( value );
+    
+    return value;
+    
   }
+
+  int read_bytes( char *buffer, int size )
+  {
+    int number = Serial.readBytes(buffer, size);
+    Serial.println( buffer );
+    
+    return number;
+    
+  }
+  
+          
+                
 
   char read()
   {
-    return Serial.read();
+    char value = Serial.read();
+
+    Serial.print( "read: " );
+    Serial.println( value );
+    
+    return value;
   }
 
   void flush()
   {
+    Serial.println( "flush" );
+    
     while( good() )
     {
       read();
@@ -76,75 +124,149 @@ class Error
 };
 
 
-VerboseSerial verbose_serial;
+VerboseSerial *verbose_serial;
 Error error_handler;
 
-
-// the setup routine runs once when you press reset:
-void setup() {                
-  error_handler.modal_blink( 0x55 );// check if it's running
-                                      // by triggering blinking
+int parse_int( char *buffer,  int &index )
+{
+  int number = 0;
+  
+  while( buffer[index] != 0 && (buffer[index] < 30 || buffer[index] > 39 ))
+  {
+    ++index;
+  }
+  
+  while( buffer[index] != 0 && buffer[index] > 30 && buffer[index] < 39 )
+  {
+    number = (number * 10) + (buffer[index] - 30);
+    ++index;
+  }
+  
+  return number;
 }
 
-
-
-
-bool handle_channel()
+bool handle_channel(char *buffer, int &index)
 {
-  int tmp_value  = verbose_serial.parse_int();
+  int tmp_value  = parse_int(buffer, index);
   active_channel = constrain( tmp_value, Red, Blue2 );
   return true;
 }
 
-bool handle_value()
+
+bool handle_value(char *buffer, int &index)
 {
-  int tmp_value = verbose_serial.parse_int();
+  int tmp_value = parse_int(buffer, index);
   color[ active_channel ] = constrain( tmp_value, 0, 255 );
   return true;
 }
 
+
+
 bool update_current_channel()
 {
+  // Serial.print( "update channel " );
+  // Serial.print( active_channel );
+  // Serial.print( " value " );
+  // Serial.println( color[active_channel] );
+  
   analogWrite( LED[active_channel],   color[active_channel] );
   return true;
 }
 
-bool parse_input()
+void handle_command( char command )
 {
-  bool went_well = false;
-
-  if( verbose_serial.good() )
+  if( command != 'n' )
   {
-    char command = verbose_serial.read();
     switch( command )
     {
     case 'c':
-    case 'C':
-      went_well = handle_channel();
+      active_channel = constrain( number, Red, Blue2 );
       break;
     case 'v':
-    case 'V':
-      went_well = handle_value();
+      color[active_channel] = constrain( number, 0, 255 );
+      update_current_channel();
       break;
     default:
-      went_well = false;
+      break;
     }
-
-    if( went_well )
-    {
-      went_well = update_current_channel();
-    }
-
-    if( !went_well )
-    {
-      error_handler.handle_error( 0x05 );
-    }
-
-    // no matter how it went, skip the rest of the string
-    verbose_serial.flush();
+    
+    number = 0;
   }
+}
+
+
+bool parse_input()
+{
+  int avail = Serial.available();
+  memset( input_buffer, 0, input_buffer_length );
+  Serial.readBytes( input_buffer, avail );
   
-  return went_well;
+  if( avail > 0 )
+  {
+    idle_counter = 0;
+    
+     // Serial.print( "Bingo " );
+     // Serial.print( avail );
+     // Serial.print( " in " );
+     // Serial.print( input_buffer );
+     // Serial.println( " KK " );
+    
+    
+    bool found   = false;
+    char new_command = 'n';
+    char c;
+    
+    for( int i=0; i<avail; ++i )
+    {
+      c = input_buffer[i];
+      if( c >= 0x30 && c<= 0x39 )
+      {
+        // digit
+        int d = c - 0x30;
+        number = number * 10 + d;
+      }
+      else
+      {
+        new_command = c;
+        found = true;
+      }
+      
+      if( found )
+      {
+        handle_command( command );
+        command = new_command;
+        found = false;
+      }
+    }
+  }
+  else
+  {
+    ++idle_counter;
+    if( idle_counter == 1 )
+    {
+     // Serial.print( "Bingo " );
+     // Serial.print( avail );
+     // Serial.print( " in " );
+     // Serial.println( " KK " );
+      handle_command( command );
+      command = 'n';
+      
+    }
+  }
+}
+
+// the setup routine runs once when you press reset:
+void setup() {        
+  memset( input_buffer, 0, input_buffer_length - 1 );
+  
+  verbose_serial = new VerboseSerial();
+
+  command = 'n';
+  number  = 0;
+  idle_counter = 0;
+
+  error_handler.modal_blink( 0x55 );// check if it's running
+                                    // by triggering blinking
 }
 
 // the loop routine runs over and over again forever:
--- gui/rgb_control.py
+++ gui/rgb_control.py
@@ -18,9 +18,12 @@ import os, sys
 import serial
 
 from gi.repository import Gtk
+from gi.repository import Gdk
 from gi.repository import GObject
 
 from threading import Thread
+
+Gdk.threads_init()
 
 SPEED = 9600
 
@@ -54,8 +57,8 @@ class MainWindow:
 
     def channel_changed(self, adjustment):
 
-        self.send_command("c %i" % adjustment.channel)
-        self.send_command("v %i" % adjustment.get_value())
+        self.send_command("c%i" % adjustment.channel)
+        self.send_command("v%i" % adjustment.get_value())
 
 
     def connect_serial(self, button):
@@ -76,7 +79,7 @@ class MainWindow:
 
         try:
             port = self.builder.get_object('serialPort').get_text()
-            self.connection = serial.Serial(port, SPEED, timeout=0, stopbits=serial.STOPBITS_TWO)
+            self.connection = serial.Serial(port, SPEED, timeout=0, stopbits=serial.STOPBITS_ONE)
             button.set_label("Disconnect")
             set_sensitive(True)
         except serial.serialutil.SerialException, e:
@@ -94,23 +97,28 @@ class MainWindow:
 
     def receive_data(self):
 
-        textview = self.builder.get_object('textview1')
-        data_buffer = Gtk.TextBuffer()
-        textview.set_buffer(data_buffer)
-
-        scrollbar = self.builder.get_object('scrolledwindow1').get_vadjustment()
-
-        last_received = ""
         buffer = ""
         while True:
             if self.connection:
                 buffer += self.connection.read(self.connection.inWaiting())
-                if '\n' in buffer:
-                    last_received, buffer = buffer.split('\n')[-2:]
-                    data_buffer.insert(data_buffer.get_end_iter(), last_received)
-                    scrollbar.set_value(adj.upper - adj.page_size)
+                buffer_list = buffer.split('\n')
+                for line in map(lambda x: x.replace("\r", ""), buffer_list[:-1]):
+                    GObject.idle_add(self.update_data_buffer, line)
+                    print line
+                buffer = buffer_list[-1]
             if self.quitting:
                 break
+
+
+    def update_data_buffer(self, data):
+
+        textview = self.builder.get_object('textview1')
+        data_buffer = Gtk.TextBuffer()
+        textview.set_buffer(data_buffer)
+        scrollbar = self.builder.get_object('scrolledwindow1').get_vadjustment()
+
+        data_buffer.insert(data_buffer.get_end_iter(), data)
+        #scrollbar.set_value(scrollbar.upper - scrollbar.page_size)
 
 
 if __name__ == '__main__':
@@ -121,3 +129,4 @@ if __name__ == '__main__':
 
     MainWindow()
     Gtk.main()
+    
